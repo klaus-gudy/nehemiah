a731a985ff79d493a254d2e38b22afbae12ec26f
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2021-09-03 08:08:26 +0300

    none

--- devs/script.js
+++ devs/script.js
@@ -1,8 +1,9 @@
 const cont = document.querySelector('.cont');
 const seats = document.querySelectorAll('.seat:not(.occupied)');
+const select = document.getElementsbyName('selected');
 
 cont.addEventListener('click', (e) => {
-  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+  if(e.target.classList.contains('seat') &&  !e.target.classList.contains('occupied')){
     e.target.classList.toggle('selected');
   }
 })
